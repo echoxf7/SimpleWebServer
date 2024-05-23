7a5bebacaca8348fa85042e9734ef1efe6fc2180
echoxf7 <echoxf7@gmail.com>
2024-05-23 01:31:07 -0400

    second

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
+// http headers
+
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs")
 const PORT = 8000;
-const url = `http://localhost:${PORT}`; 
-
+const url = `http://localhost:${PORT}`
 const app = express();
+app.get("/api/users",(req, res) => {
+  fs.readFile('../data.json', "utf-8", (err, data) =>{
+      res.send(data)
+  })
+})
 
-app.get('/api/', (req, res) => {
-    fs.readFile("data.json", "utf-8", (err, data) => {
-        if (err) {
-            res.status(500).send("Error reading file");
-            return;
-        }
-        res.send(data);
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`The server started at: ${url}`);
+app.listen(PORT, () =>{
+  console.log(`The server is running at ${url}`)
 });
